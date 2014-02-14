028599b2137a7e64ea6d5aa99d56f5d390cc71e0
kevinchap <kevin.chapron@hetic.net>
2014-02-14 14:25:26 +0100

    MAJ Kévin : (MAJ)app_controller(modifyMDP), app_model

--- tmp/0hac6cxw48f.0zgfl3xpxf2.php
+++ tmp/0hac6cxw48f.0zgfl3xpxf2.php
@@ -10,7 +10,9 @@
 <body class="clearfix">
 	<p>Layout</p>
 <?php echo $this->render('partials/header.htm',$this->mime,get_defined_vars()); ?>
-<?php echo $this->render($content,$this->mime,get_defined_vars()); ?>
+
+<?php echo $content; ?>
+
 <?php echo $this->render('partials/footer.htm',$this->mime,get_defined_vars()); ?>
 </body>
 </html>
