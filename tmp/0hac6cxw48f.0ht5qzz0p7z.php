f68bd40a79bbe7cd7c50857acfaf71b278aa4cc6
kevinchap <kevin.chapron@hetic.net>
2014-02-22 15:17:51 +0100

    MAJ Kévin : minor correct

--- tmp/0hac6cxw48f.0ht5qzz0p7z.php
+++ tmp/0hac6cxw48f.0ht5qzz0p7z.php
@@ -5,3 +5,9 @@
 <a href="profil">Profil</a>
 <br/>
 <a href="maCave">Cave</a>
+<br/>
+<a href="otherUsers">Les autres</a>
+
+<?php echo $this->render('./partials/SearchWine.htm',$this->mime,get_defined_vars()); ?>
+<?php echo $this->render('./partials/WineShuffle.htm',$this->mime,get_defined_vars()); ?>
+<?php echo $this->render('./partials/LastUsrFavoritWine.htm',$this->mime,get_defined_vars()); ?>
--- tmp/0hac6cxw48f.0w80fbzsp71.php
+++ tmp/0hac6cxw48f.0w80fbzsp71.php
@@ -1,12 +1,15 @@
 <p>Ma cave</p>
 <a href="addWine">Ajouter un vin !</a>
 <span style="color:<?php echo $color; ?>;"><?php echo $message; ?></span>
+<br/>
 <div class="wine">
 	<?php foreach (($resultat?:array()) as $item): ?>
-		<div style="border:1px solid black;">
+		<div style="border:1px solid black;width:350px;display:inline-block;vertical-align:top;">
 			<span>Nom : <?php echo $item['wine_name']; ?></span>
 		    <br/>
-		    <span>Date d'ajout : <?php echo $item['wine_date_add']; ?></span>
+		    <span>Date d'ajout : <?php echo $item['wine_time_add']; ?></span>
+		    <br/>
+		    <span>Propriétaire : <?php echo $proprietaire; ?></span>
 		    <br/>
 		    <div class="img" style="height:100px;width:100px;overflow:hidden;">
 		    	<img style="height:120px;width:120px;" src="./avatars/<?php echo $item['wine_img']; ?>" alt="<?php echo $item['wine_img']; ?>"/>
@@ -22,6 +25,9 @@
 		    <br/>
 		    <span>Commentaires et/ou conseil de dégustation : </span><br/>
 		    <span><?php echo $item['wine_conseil']; ?></span>
+		    <br/>
+		    <a href="delete/<?php echo $item['wine_id']; ?>">Supprimer le vin</a>
+		    <a href="modifyWine/<?php echo $item['wine_id']; ?>">Modifier le vin</a>
 		</div>
 	<?php endforeach; ?>
 </div>
