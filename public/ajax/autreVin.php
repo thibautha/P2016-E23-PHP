b31c75ed2b780aa6ea186601a335cb235fb5fbc6
Ameziane Allioui <ameziane.allioui@gmail.com>
2014-03-07 04:19:21 +0100

    MAJ bouton "autre-vin" -> nouveau titre OK

--- public/ajax/autreVin.php
+++ public/ajax/autreVin.php
@@ -8,13 +8,14 @@ $con = mysqli_connect("localhost","root","","alanotre");
 //$bdd = new PDO('mysql:host=preprod.hetic.net;dbname=alanotre', 'alanotre', 'laNOtr3458');
 
 //Requête faite à la base de données 
-$query = 'SELECT * FROM userwine ';
-// $query = 'SELECT * FROM wine WHERE wine_name!="'.$nomVinActuel.'" ORDER BY RAND() LIMIT 1';
+$query = 'SELECT * FROM wine WHERE wine_name!="'.$nomVinActuel.'" ORDER BY RAND() LIMIT 1';
 
 // On récupère les informations sur le vin sous format json
-//$results = json_encode($bdd->query($query));
+
 $autreVin = $con->query($query);
 
-echo json_encode($autreVin);
+$autreVin = $autreVin->fetch_object();
 
+//on encode les résultats en json et on les renvoie à notre fonction en ajax
+echo json_encode($autreVin);
 ?>
--- public/js/alanotre.js
+++ public/js/alanotre.js
@@ -55,18 +55,28 @@ $(function() {
 
 	/* UN AUTRE VIN */
 
+	//Lorsqu'on clique sur le bouton "autre vin"
 	$('.autre-vin').on('click', function(){
-	
+		
+		// On récupère son nom
 		var nomVinActuel = $("#random h3").html();
 
+		/*On éxécute une requête asynchrone pour nous afficher un autre vin */
+		
 		$.ajax({   
 		   type: "GET",
+		   contentType: 'application/json; charset=UTF-8',
 		   url: "./public/ajax/autreVin.php",
 		   data: {nomVinActuel : nomVinActuel},
 		   dataType:'json',
 		   success:function(autreVin){
-		   		console.log(autreVin);
-		   		//nomVinActuel.empty().html(listeVins);
+		   		var vinId = autreVin["wine_id"];
+            	$("#random h3").animate('easeOut', 1000).replaceWith('<h3>'+autreVin["wine_name"]+'</h3>'); 
+            	vinImg = autreVin["wine_img"],
+            	vinO = autreVin["wine_origine"]
+            	vinC = autreVin["wine_cepage"]
+            	vinM = autreVin["wine_millesime"];
+
 		   }
 		});
 	});
