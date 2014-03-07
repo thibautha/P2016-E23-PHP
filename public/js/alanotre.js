72635b2331193700bcd9c6053faf87912d534d56
Ameziane Allioui <ameziane.allioui@gmail.com>
2014-03-07 07:48:38 +0100

    maj alanotre.js

--- public/js/alanotre.js
+++ public/js/alanotre.js
@@ -1,58 +1,5 @@
 $(function() {
-	/*var availableTags = [
-		"ActionScript",
-		"AppleScript",
-		"Asp",
-		"BASIC",
-		"C",
-		"C++",
-		"Clojure",
-		"COBOL",
-		"ColdFusion",
-		"Erlang",
-		"Fortran",
-		"Groovy",
-		"Haskell",
-		"Java",
-		"JavaScript",
-		"Lisp",
-		"Perl",
-		"PHP",
-		"Python",
-		"Ruby",
-		"Scala",
-		"Scheme"
-	];*/
-
-	/*
-
-	AUTOCOMPLETION
-
-	var vinsDisponibles;
-
-	$.ajax({
-        type: "POST",
-        url: "./public/ajax/vinsDisponibles.php",
-        dataType: "json",
-        data:{vinsDisponibles : },
-        success: function(data, status) {
-
-        },
-      	error: function(xhr, desc, err) {
-        	console.log(xhr);
-       		console.log("Details: " + desc + "\nError:" + err);
-        }
-    });
-
-	var $searchWine = $('#search-wine');
-
-
-	$searchWine.on('focus', function(){
-		$(this).autocomplete({
-			source: vinsDisponibles
-		});
-	});*/
-
+	
 	/* UN AUTRE VIN */
 
 	//Lorsqu'on clique sur le bouton "autre vin"
